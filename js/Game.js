faf7a42567c946d5b74d031580b3609f8a074275
BellaBradbury <bellabradbury@yahoo.com>
2023-02-27 12:57:58 -0500

    phrases populated

--- js/Game.js
+++ js/Game.js
@@ -6,7 +6,13 @@
 class Game {
     constructor() {
         this.missed = 0;
-        this.phrases = [];
+        this.phrases = [
+            {phrase: 'My great adventure has begun'},
+            {phrase: 'Some things are meant to be'},
+            {phrase: 'Do not try to make me grow up before my time'},
+            {phrase: 'I have lived for loving you'},
+            {phrase: 'I will shine as brightly as the sun'}
+        ];
         this.activePhrase = null;
     }
 } 
--- js/app.js
+++ js/app.js
@@ -2,5 +2,7 @@
  * Project 4 - OOP Game App
  * app.js */
 
-const phrase = new Phrase('Life is like a box of chocolates');
-console.log(`Phrase - phrase: ${phrase.phrase}`);
+const game = new Game();
+game.phrases.forEach((phrase, index) => {
+console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+});
